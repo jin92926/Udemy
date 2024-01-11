13069a99b18a79ce353d729b93641e182048fdca
jin92926 <jin92926@gmail.com>
2024-01-12 08:31:15 +0900

    240112 practice

--- Algorithm/maxSubarraySum.js
+++ Algorithm/maxSubarraySum.js
@@ -6,3 +6,18 @@
 // maxSubarraySum([-3,4,0,-2,6,-1], 2) // 5
 // maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2) // 5
 // maxSubarraySum([2,3], 3) // null
+
+const maxSubarraySum = (arr, num) => {
+  let [maxSum, tempSum] = [0, 0];
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+
+  return maxSum;
+};
