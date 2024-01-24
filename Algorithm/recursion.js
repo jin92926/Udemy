688467365677ed1adf9d51f96250d0b157f39c7b
jin92926 <jin92926@gmail.com>
2024-01-24 09:54:31 +0900

    240124 practice

--- Algorithm/recursion.js
+++ Algorithm/recursion.js
@@ -35,3 +35,16 @@ const sumRange2 = (num) => {
   }
   return result;
 };
+
+const factorial = (num) => {
+  let total = 1;
+  for (let i = num; i > 0; i--) {
+    total *= i;
+  }
+  return total;
+};
+
+const factorial2 = (num) => {
+  if (num === 1) return 1;
+  return num * factorial2(num - 1);
+};
