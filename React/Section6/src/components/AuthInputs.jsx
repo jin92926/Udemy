da9b936b55a1d50dd70bd92c2431b1c5d5cc6f19
jin92926 <jin92926@gmail.com>
2024-02-04 10:05:02 +0900

    240204 practice

--- React/Section6/src/components/AuthInputs.jsx
+++ React/Section6/src/components/AuthInputs.jsx
@@ -1,12 +1,55 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { styled } from "styled-components";
+
+// tagged templates을 입력으로 받음
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+const ControlLabel = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: ${({ invalid }) => (invalid ? "#f87171" : "#6b7280")};
+`;
+
+// const ControlLabel = styled.label.attrs((props) => ({
+//   style: {
+//     color: props.invalid ? "#f87171" : "#6b7280",
+//   },
+// }))`
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   font-size: 0.75rem;
+//   font-weight: 700;
+//   letter-spacing: 0.1em;
+//   text-transform: uppercase;
+// `;
+
+const ControlInput = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: #d1d5db;
+  color: #374151;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
 
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
+    if (identifier === "email") {
       setEnteredEmail(value);
     } else {
       setEnteredPassword(value);
@@ -17,36 +60,48 @@ export default function AuthInputs() {
     setSubmitted(true);
   }
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
+  // console.log(emailNotValid.toString());
   return (
     <div id="auth-inputs">
-      <div className="controls">
+      <ControlContainer>
         <p>
-          <label>Email</label>
-          <input
+          {/* 바닐라css를 섞어서 사용할 수 있음 */}
+          {/* <ControlLabel className={`label ${emailNotValid ? "invalid" : ""}`}> */}
+          <ControlLabel invalid={emailNotValid}>Email</ControlLabel>
+          <ControlInput
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
+            //인라인 스타일
+            // style={{
+            //   background: emailNotValid ? "red" : "#d1d5db",
+            // }}
+
+            //이 방법도 가능한 것처럼 보이지만 재로딩하면 에러
+            // className={emailNotValid && "invalid"}
+            className={emailNotValid ? "invalid" : undefined}
+            onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
-          <input
+          <ControlLabel>Password</ControlLabel>
+          <ControlInput
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            className={passwordNotValid ? "invalid" : undefined}
             onChange={(event) =>
-              handleInputChange('password', event.target.value)
+              handleInputChange("password", event.target.value)
             }
           />
         </p>
-      </div>
+      </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <button className="button" onClick={handleLogin}>
+          Sign In
+        </button>
       </div>
     </div>
   );
--- React/Section6/src/components/Header.jsx
+++ React/Section6/src/components/Header.jsx
@@ -3,7 +3,13 @@ import logo from "../assets/logo.png";
 // 바닐라css의 단점은 해당 컴포넌트로 그 규칙이 스코핑되어있지않아
 // 다른 컴포넌트와 스타일 충돌이 발생할 수 있음
 
-import "./Header.css";
+// import "./Header.css";
+
+//css모듈화 하는 방법
+//module을 붙이고 네이밍은 자유롭게 할 수 있음, 객체처럼 가져와서 사용
+//개발자도구에 들어가면 기존의 클래스 이름과 다른데 빌드툴에 의해 자동으로 생성
+//이렇게 하면 컴포넌트에서 독립적으로 사용 가능
+import classes from "./Header.module.css";
 
 export default function Header() {
   return (
@@ -13,9 +19,10 @@ export default function Header() {
       <p
         // 컴포넌트에 스코핑되는 스타일을 사용하는 방법 중 하나는 인라인 속성을 이용하는 것
         // 장점은 하나의 컴포넌트에만 적용 가능하다는 점이며 단점은 장점과 동일
-        style={{
-          color: "red",
-        }}
+        // style={{
+        //   color: "red",
+        // }}
+        className={classes.paragraph}
       >
         A community of artists and art-lovers.
       </p>
