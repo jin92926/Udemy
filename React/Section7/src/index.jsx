8a9b093b914de09160639e5c2998f29fed980b4f
jin92926 <jin92926@gmail.com>
2024-02-14 23:48:31 +0900

    240214 practice

--- React/Section7/src/index.jsx
+++ React/Section7/src/index.jsx
@@ -1,6 +1,12 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 
-import App from './App.jsx';
-import './index.css';
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  //개발모드에서 모든 컴포넌트 함수를 두번 실행함,
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
