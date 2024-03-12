b10a50e074ac4770ade48038645cd1d03a08404e
jin92926 <jin92926@gmail.com>
2024-03-12 09:20:21 +0900

    240312 practice

--- Algorithm/QuickSort/quickSort.js
+++ Algorithm/QuickSort/quickSort.js
@@ -1,2 +1,35 @@
 // 퀵정렬은 합병 정렬과 같은 가정으로 작동
 // 피벗포인트라 불리는 단일 요소를 선택해 수행
+// 정렬된 배열에서 최소값만 선택할 경우 O(n^2) 발생할 수 있으니 중간값이나
+// 랜덤값을 선택하는 것도 방법
+
+const pivot = (arr, start = 0, end = arr.length + 1) => {
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+  swap(arr, start, swapIdx);
+  return swapIdx;
+};
+const swap = (arr, i, j) => {
+  //   let temp = arr[i];
+  //   arr[i] = arr[j];
+  //   arr[j] = temp;
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+};
+
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+};
