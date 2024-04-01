1862762c30dd1640a4f10e5425d81d754f91b6a2
jin92926 <jin92926@gmail.com>
2024-04-01 15:33:39 +0900

    240401 practice

--- Algorithm/RadixSort/radixsort.js
+++ Algorithm/RadixSort/radixsort.js
@@ -14,3 +14,15 @@ const mostDigits = (nums) => {
   }
   return maxDigits;
 };
+
+const radixSort = (nums) => {
+  let maxDigitCount = mostDigits(nums);
+  for (let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < nums.length; i++) {
+      digitBuckets[getDigit(num[i], k)].push(nums[i]);
+    }
+    nums = [].concat(...digitBuckets);
+  }
+  return nums;
+};
