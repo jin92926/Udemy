9cad76da90e3e24596f07dd6bcd08975ca84b165
jin92926 <jin92926@gmail.com>
2024-04-13 10:33:54 +0900

    240413 practice

--- Algorithm/DataStructures/class.js
+++ Algorithm/DataStructures/class.js
@@ -3,6 +3,26 @@ class Student {
     this.firstName = firstName;
     this.lastName = lastName;
   }
+  fullName() {
+    return `Your full name is ${this.firstName} ${this.lastName}`;
+  }
+  markLate() {
+    this.tardies += 1;
+    if (this.tardies >= 3) {
+      return "YOU ARE EXPELLED!!!!";
+    }
+    return `${this.firstName} ${this.lastName} has been late ${this.tardies} times`;
+  }
+  addScore(score) {
+    this.scores.push(score);
+    return this.scores;
+  }
+  calculateAverage() {
+    let sum = this.scores.reduce(function (a, b) {
+      return a + b;
+    });
+    return sum / this.scores.length;
+  }
 }
 
 let firstStudent = new Student("Colt", "steele");
