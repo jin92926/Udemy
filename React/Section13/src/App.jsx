3e229cfc603871b2a3d55e6131686c278b40eb40
jin92926 <jin92926@gmail.com>
2024-04-18 09:02:07 +0900

    240418 practice

--- React/Section13/src/App.jsx
+++ React/Section13/src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Counter from './components/Counter/Counter.jsx';
-import Header from './components/Header.jsx';
-import { log } from './log.js';
+import Counter from "./components/Counter/Counter.jsx";
+import Header from "./components/Header.jsx";
+import { log } from "./log.js";
+import ConfigureCounter from "./components/Counter/ConfigureCounter.jsx";
 
 function App() {
-  log('<App /> rendered');
+  log("<App /> rendered");
 
-  const [enteredNumber, setEnteredNumber] = useState(0);
   const [chosenCount, setChosenCount] = useState(0);
 
-  function handleChange(event) {
-    setEnteredNumber(+event.target.value);
-  }
-
-  function handleSetClick() {
-    setChosenCount(enteredNumber);
-    setEnteredNumber(0);
+  function handleSetClick(newCount) {
+    setChosenCount(newCount);
   }
 
   return (
     <>
       <Header />
       <main>
-        <section id="configure-counter">
+        {/* input를 여기 두면 바뀔때마다 app 내에 있는 모든 코드들이 재랜더링됨
+          1. memo를 활용해서 input과 관련없는 Counter에 적용해 한번만 실행되게 하기
+          2.input를 다른 컴포넌트로 분리하기 <- 이 방법이 효율적임
+          */}
+        {/* <section id="configure-counter">
           <h2>Set Counter</h2>
           <input type="number" onChange={handleChange} value={enteredNumber} />
           <button onClick={handleSetClick}>Set</button>
-        </section>
+        </section> */}
+        <ConfigureCounter onSet={handleSetClick} />
         <Counter initialCount={chosenCount} />
       </main>
     </>
--- React/Section13/src/components/Counter/Counter.jsx
+++ React/Section13/src/components/Counter/Counter.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react';
+// memo는 재랜더링을 막기 위해 사용하지만 남용해서는 안됨
+// 되도록 상위컴포넌트 트리에 있는 컴포넌트에만 사용해야함
+// 그 이유는 memo는 받아온 속성값이 바뀔때만 재랜더링이 되는데
+// 이것은 속성값을 확인하기 때문에 성능에 문제를 일으킬 수 있음
 
-import IconButton from '../UI/IconButton.jsx';
-import MinusIcon from '../UI/Icons/MinusIcon.jsx';
-import PlusIcon from '../UI/Icons/PlusIcon.jsx';
-import CounterOutput from './CounterOutput.jsx';
-import { log } from '../../log.js';
+import { memo, useState } from "react";
+
+import IconButton from "../UI/IconButton.jsx";
+import MinusIcon from "../UI/Icons/MinusIcon.jsx";
+import PlusIcon from "../UI/Icons/PlusIcon.jsx";
+import CounterOutput from "./CounterOutput.jsx";
+import { log } from "../../log.js";
 
 function isPrime(number) {
-  log(
-    'Calculating if is prime number',
-    2,
-    'other'
-  );
+  log("Calculating if is prime number", 2, "other");
   if (number <= 1) {
     return false;
   }
@@ -27,8 +28,8 @@ function isPrime(number) {
   return true;
 }
 
-export default function Counter({ initialCount }) {
-  log('<Counter /> rendered', 1);
+const Counter = memo(function Counter({ initialCount }) {
+  log("<Counter /> rendered", 1);
   const initialCountIsPrime = isPrime(initialCount);
 
   const [counter, setCounter] = useState(initialCount);
@@ -44,8 +45,8 @@ export default function Counter({ initialCount }) {
   return (
     <section className="counter">
       <p className="counter-info">
-        The initial counter value was <strong>{initialCount}</strong>. It{' '}
-        <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
+        The initial counter value was <strong>{initialCount}</strong>. It{" "}
+        <strong>is {initialCountIsPrime ? "a" : "not a"}</strong> prime number.
       </p>
       <p>
         <IconButton icon={MinusIcon} onClick={handleDecrement}>
@@ -58,4 +59,6 @@ export default function Counter({ initialCount }) {
       </p>
     </section>
   );
-}
+});
+
+export default Counter;
