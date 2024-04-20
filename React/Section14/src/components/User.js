b1e3eda0f902adf15f46cfab9ccb7a42b2d56806
jin92926 <jin92926@gmail.com>
2024-04-20 09:31:43 +0900

    240420 practice

---
+++ React/Section14/src/components/User.js
@@ -0,0 +1,16 @@
+import classes from "./User.module.css";
+import { Component } from "react";
+
+// render는 props를 받지않음, Component 사용해서 props에 접근할 수 있음
+class User extends Component {
+  //리액트에 필요한 메서드
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+export default User;
