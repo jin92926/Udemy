68fc54c701e591dbb0f51502061c87f6985cb96c
jin92926 <jin92926@gmail.com>
2024-05-05 11:41:27 +0900

    240505 practice

--- React/Section16/src/hooks/useFetch.js
+++ React/Section16/src/hooks/useFetch.js
@@ -1,10 +1,34 @@
 // react에서 use로 시작하는 함수는 훅으로 인식되며, 특정 규칙을 부여함
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // 커스텀 훅은 use로 시작해야함
-const useFetch = () => {
-  useEffect();
+const useFetch = (fetchFn) => {
+  const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState();
+  const [fetchedData, setFetchedData] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const places = await fetchFn();
+        setUserPlaces(places);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch data." });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchFn]);
+
+  return {
+    isFetching,
+    fetchedData,
+    error,
+  };
 };
 
 export default useFetch;
