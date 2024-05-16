3acd2e54d524873ebb4e479ad2acb455672960fa
jin92926 <jin92926@gmail.com>
2024-05-17 08:40:41 +0900

    240517 practice

--- React/Section18/src/components/Header.jsx
+++ React/Section18/src/components/Header.jsx
@@ -1,7 +1,14 @@
+import { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
 
 const Header = () => {
+  const cartCtx = useContext(CartContext);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
   return (
     <header id="main-header">
       <div id="title">
@@ -10,7 +17,7 @@ const Header = () => {
       </div>
       <nav>
         {/* 리액트는 자동으로 속성을 true로 설정하기에 굳이 안넣어도 됨 */}
-        <Button textOnly>Cart (0)</Button>
+        <Button textOnly>Cart ({totalCartItems})</Button>
       </nav>
     </header>
   );
---
+++ React/Section18/src/store/CartContext.jsx
@@ -0,0 +1,87 @@
+import { createContext, useReducer } from "react";
+
+//컨텍스트 객체 생성
+const CartContext = createContext({
+  items: [],
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const updatedItems = [...state.items];
+
+    if (existingCartItemIndex > -1) {
+      const existingItem = state.items[existingCartItemIndex];
+      const updatedItem = {
+        ...existingItem,
+        quantity: state.items[existingCartItemIndex].quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({ ...action.item, quantity: 1 });
+    }
+    return { ...state, items: updatedItems };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedItems = [...state.items];
+
+    if (existingCartItem.quantity === 1) {
+      updatedItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return { ...state, items: updatedItems };
+  }
+
+  return state;
+};
+
+// 컨텍스트 제공, 실제 상태 관리
+export const CartContextProvider = ({ children }) => {
+  //useState도 사용 가능하지만 값이 복잡하기에 useReducer를 사용하는 게 좋음
+  //두번째 인자로 초기 상태값을 보내고 개발자가 모양을 정의함
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
+
+  const addItem = (item) => {
+    dispatchCartAction({
+      type: "ADD_ITEM",
+      item,
+    });
+  };
+
+  const removeItem = (id) => {
+    dispatchCartAction({
+      type: "REMOVE_ITEM",
+      id,
+    });
+  };
+
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem,
+  };
+
+  console.log(cartContext);
+
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
+};
+
+export default CartContext;
