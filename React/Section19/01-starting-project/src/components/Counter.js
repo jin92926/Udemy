a74c88da3c851b76d769b7f75758d7bcdacd3b0b
jin92926 <jin92926@gmail.com>
2024-06-09 08:26:59 +0900

    240609 practice

--- React/Section19/01-starting-project/src/components/Counter.js
+++ React/Section19/01-starting-project/src/components/Counter.js
@@ -1,10 +1,19 @@
 import classes from "./Counter.module.css";
 // useStore로 저장소에 직접 액세스 할 수 있지만 useSelector가 더 사용하기 편함
 // 저장소가 관리하는 상태 부분을 자동으로 선택할 수 있기 때문임
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -12,6 +21,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
