e6606e8655ce31214dec8975820a483b76d95fc6
jin92926 <jin92926@gmail.com>
2024-06-14 23:44:27 +0900

    240614 practice

--- React/Section19/01-starting-project/src/store/index.js
+++ React/Section19/01-starting-project/src/store/index.js
@@ -1,23 +1,43 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
+  //아래와 같이 절대 원본 기존 상태를 변경해서는 안됨
+  //항상 새로운 객체를 반환해서 재정의해야함
+  // if (action.type === "increment") {
+  //   state.counter ++
+  //   return state
+  // }
+
   if (action.type === "increment") {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "increase") {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
     };
   }
+
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
+    };
+  }
+
   return state;
 };
 
