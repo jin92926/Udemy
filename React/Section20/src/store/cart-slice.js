60540a859f13a18076da65a8c1edc2c815fbd4f5
jin92926 <jin92926@gmail.com>
2024-06-21 08:41:34 +0900

    240621 practice

--- React/Section20/src/store/cart-slice.js
+++ React/Section20/src/store/cart-slice.js
@@ -1,6 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
+//비동기코드를 절대 리듀서 안에서 실행하면 안됨
+//사용방법은 2가지가 있음
+//1. 컴포넌트 안에서
+//2. 액션 생성 함수
+const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
@@ -10,10 +14,11 @@ createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
       if (!existingItem) {
         //리덕스 툴킷 내부적으로 기존 상태를 변경안하기에 push 사용 가능
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -21,9 +26,23 @@ createSlice({
         });
       } else {
         existingItem.quantity++;
-        existingItem.totalPrice += newItem.price;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart() {},
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
