3a6c23fbe05afc462e8f1652f5c6be3b31021fe2
jin92926 <jin92926@gmail.com>
2024-06-24 00:26:52 +0900

    240624 practice

--- React/Section20/src/App.js
+++ React/Section20/src/App.js
@@ -2,10 +2,16 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
+import app from "./firebase";
 
 function App() {
   //상태를 자동으로 받는 함수를 전달해야함
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+
+  console.log(app);
+
+  // const test = app.collection("test");
+  // console.log(test);
 
   return (
     <Layout>
