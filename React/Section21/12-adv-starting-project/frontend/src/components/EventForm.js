4833d41479cfec7f37ae4cdba6972e780cdbec35
jin92926 <jin92926@gmail.com>
2024-07-06 23:57:55 +0900

    240706 practice

--- React/Section21/12-adv-starting-project/frontend/src/components/EventForm.js
+++ React/Section21/12-adv-starting-project/frontend/src/components/EventForm.js
@@ -1,8 +1,17 @@
-import { useNavigate, Form, useNavigation } from "react-router-dom";
+import {
+  useNavigate,
+  Form,
+  useNavigation,
+  useActionData,
+} from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
+  //useLoaderData와 비슷한 기능을 함, action이 리턴한 데이터에 액세스할 수 있게 해줌
+  //가장 가까운 action에 대한 액세스를 제공
+  const data = useActionData();
+
   const navigate = useNavigate();
   const navigation = useNavigation();
 
@@ -15,6 +24,13 @@ function EventForm({ method, event }) {
   return (
     //Form을 쓰면 action으로 전송됨
     <Form method="post" className={classes.form}>
+      {data && data.errors && (
+        <ul>
+          {Object.values(data.errors).map((err) => (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>
+      )}
       <p>
         <label htmlFor="title">Title</label>
         <input
