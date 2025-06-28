adb3914c639a279b35b4c31c139b87d72691614d
jin92926 <jin92926@gmail.com>
2025-06-28 23:44:23 +0900

    250628 practice

--- Algorithm/section5/multiplePointers.js
+++ Algorithm/section5/multiplePointers.js
@@ -11,6 +11,7 @@ function sumZero(arr) {
 
 sumZero([-4, -3, -2, -1, 0, 1, 2, 5]);
 
+//O(n), 배열은 정렬되어 있음
 function sumZero2(arr) {
   let left = 0;
   let right = arr.length - 1;
@@ -26,8 +27,12 @@ function sumZero2(arr) {
   }
 }
 
+//중복되지 않는 고유한 숫자값이 몇 개 있는지 반환하는 함수 작성
 // 객체로 만든 후에 키값 길이로
 function countUniqueValues(arr) {
+  //초기 조건 길이가 0이라면 0
+  if (arr.length === 0) return 0;
+
   const obj = arr.reduce((acc, cur) => {
     acc[cur] = (acc[cur] || 0) + 1;
     return acc;
@@ -37,7 +42,15 @@ function countUniqueValues(arr) {
   return result;
 }
 
-// 기존 풀이
+countUniqueValues([1, 1, 1, 2, 2, 2, 2, 3]);
+
+function countUniqueValues2(arr) {
+  if (arr.length === 0) return 0;
+  //Set은 중복되지 않는 유일한 값들만 저장하는 컬렉션 객체로 이용하면 훨씬 간편함
+  return new Set(arr).size;
+}
+
+// 기존 풀이, 포인터 방식이 코드는 더 길지만 정렬되어 있다면 공간 복잡도가 O(1)으로 공간 효율이 좋음
 function countUniqueValues2(arr) {
   // add whatever parameters you deem necessary - good luck!
   if (arr.length === 0) return 0;
--- Algorithm/section5/slidingWindow.js
+++ Algorithm/section5/slidingWindow.js
@@ -1,7 +1,6 @@
 function maxSubarraySum(arr, num) {
-  if (num > arr.length) {
-    return null;
-  }
+  if (num > arr.length) return null;
+
   var max = -Infinity;
   for (let i = 0; i < arr.length - num + 1; i++) {
     temp = 0;
@@ -21,11 +20,13 @@ function maxSubarraySum2(arr, num) {
   let maxSum = 0;
   let tempSum = 0;
   if (arr.length < num) return null;
+  //num갯수만큼의 초기 합한 수를 구함
   for (let i = 0; i < num; i++) {
     maxSum += arr[i];
   }
   tempSum = maxSum;
   for (let i = num; i < arr.length; i++) {
+    //그 후 합한 배열 첫번째 수를 빼고 합한 배열 끝에서 하나 뒤에 있는 값을 더함
     tempSum = tempSum - arr[i - num] + arr[i];
     maxSum = Math.max(maxSum, tempSum);
   }
